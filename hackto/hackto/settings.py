ed87c9700e4dd0bdbbbeafe816a266a553cd213e
Arnold Masanga <masaar01@luther.edu>
2022-09-13 08:47:49 -0400

    add gitignore

--- hackto/hackto/settings.py
+++ hackto/hackto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@4@0t3&vb!@(3^k#1k8386jq-%=yd51($@wc-qve-t#8_p+&o^"
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
